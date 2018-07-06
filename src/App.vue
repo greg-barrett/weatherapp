beffd9eac1a7f57a16737953e0ff9640d006984e
Gregory Barrett <gbarrett15@gmail.com>
2018-07-06 16:11:31 +0200

    to heroku

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div  :class="classChanger()" id="app">
+    <weather v-on:changedTemp="updateTemp($event)"></weather>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Weather
+  },
+  data() {
+    return {
+      temp: "",
+    }
+  },
+  methods: {
+    updateTemp(e) {
+      this.temp=e;
+    },
+    classChanger() {
+        if (this.temp < 0) {
+        return "v-cold";
+      } else if (this.temp < 10) {
+        return "cold";
+      } else if (this.temp < 15) {
+        return "mild";
+      } else if (this.temp < 20) {
+        return "warm";
+      } else if (this.temp < 25) {
+        return "hot";
+      } else if (this.temp < 30) {
+        return "v-hot";
+      } else if (this.temp >= 30) {
+        return "v-hot";
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Montserrat', sans-serif;
+  padding-top: 60px;
+  padding-bottom: 60px;
+  height: 450px;
+}
+.v-cold {
+  background: #c0c0aa;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #1cefff, #c0c0aa);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #1cefff, #c0c0aa); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+
+.cold {
+  background: #c2e59c;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #64b3f4, #c2e59c);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #64b3f4, #c2e59c); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+
+.mild {
+
+  background: #dce35b; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #dce35b, #45b649); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #dce35b, #45b649); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+.warm {
+  background: #FF5F6D;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #FFC371, #FF5F6D);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #FFC371, #FF5F6D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+.hot {
+  background: #FF4E50;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #F9D423, #FF4E50);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #F9D423, #FF4E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+}
+.v-hot {
+  background: #c21500;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #ffc500, #c21500);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #ffc500, #c21500); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
 }
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="main">
+    <input placeholder="City Name" v-model="town" v-on:keyup.enter="getWeather" v-on:keyup.backspace="clearAll" >
+    <div v-show="shown" class="forecast">
+      <p>{{town}}</p>
+      <p>{{description}}</p>
+      <i class="icon" v-bind:class="icon" ></i>
+      <p  v-on:click="tempCon">Today's High: {{high.toFixed(0)}}<span v-show="metric"> &#8451;</span><span v-show="!metric"> &#8457;</span></p>
+    </div>
+    <p class="error" v-show="!shown">{{error}}</p>
+  </div>
+</template>
+
+<script>
+let iconMap= {
+    "wi-owm-200": "Thunderstorm",
+    "wi-owm-210": "Lightning",
+    "wi-owm-300": "Drizzle",
+    "wi-owm-302": "Rain",
+    "wi-owm-313": "Showers",
+    "wi-owm-531": "Storm-showers",
+    "wi-owm-600": "Snow",
+    "wi-owm-602": "Sleet",
+    "wi-owm-711": "Smoke",
+    "wi-owm-731": "Dust",
+    "wi-owm-741": "Fog",
+    "wi-owm-781": "Tornado",
+    "wi-owm-800": "Clear",
+    "wi-owm-804": "Clouds",
+    "wi-owm-902": "Hurricane",
+    "wi-owm-904": "Hot",
+    "wi-owm-905": "Windy",
+    "wi-owm-906": "Hail",
+}
+
+export default {
+  name: 'Weather',
+  props: {
+    temp: {
+      type:String
+    }
+  },
+
+  data () {
+    return {
+      town: "",
+      data:"",
+      description:"",
+      high:0,
+      shown: false,
+      error:"",
+      metric:true,
+      symbol:"C",
+      icon:""
+    }
+  },
+  methods: {
+    sendTemp() {
+      this.$emit("changedTemp", this.high)
+    },
+    clearAll(){
+      this.shown= false;
+    },
+    capitalizeFirstLetter() {
+       this.town= this.town.charAt(0).toUpperCase() + this.town.slice(1);
+     },
+    getIcon() {
+      for (var key in iconMap) {
+        if (iconMap[key]===this.description) {
+          this.icon="wi "+ key;
+          return
+        }
+
+      }
+    },
+    async getWeather() {
+      this.capitalizeFirstLetter();
+      try {
+        let response= await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.town +'&APPID=eb298f5202793a5a6f90604be2e29842&units=metric');
+        let data= await response.json();
+        if (data.cod=="200") {
+           this.data=data;
+           this.description=data.weather[0].main;
+           this.high=data.main.temp;
+           this.shown=true;
+           this.metric=true;
+           this.getIcon();
+           this.sendTemp();
+         } else {
+           this.error="Huston we have a problem";
+           this.shown=false;
+         }
+
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    tempCon() {
+      if (this.metric) {
+        this.high=(this.high *1.8) +32
+        this.metric=false;
+      } else {
+        this.high=(this.high-32)/1.8
+        this.metric=true;
+      }
+    }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+  width: 60%;
+  margin: auto;
+  height: 400px;
+  background-color: white;
+  border-radius: 20px;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+
+}
+input {
+  width: 40%;
+  margin-top: 40px;
+  text-align: center;
+  font-size: 1.2em;
+  border: none;
+  border-bottom: 3px solid gray;
+  padding-bottom: 5px;
+}
+
+.forecast {
+  text-align: center;
+  font-size: 1.5em;
+
+}
+.icon {
+  font-size: 4em;
+
+}
+.error {
+  text-align: center;
+  padding-top: 50px;
+}
+</style>
+<style src="../../css/weather-icons.css"></style>
